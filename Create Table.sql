75f4e3c852df3881a968d2e96e6d777dd7f0fa02
Zhenru Huang <zhuang4@mail.sfsu.edu>
2017-11-26 19:34:11 -0800

    Update Create Table.sql

--- Create Table.sql
+++ Create Table.sql
@@ -1,3 +1,6 @@
+/* Use bookstore database*/
+use bookstore;
+
 /*　Entity */
 
 create table Author(
@@ -5,7 +8,7 @@ create table Author(
 	author_name varchar(255), 
 	author_birth date,
 	primary key (author_id)
-)
+);
 
 create table Book(
 	book_id int not null,
@@ -13,13 +16,13 @@ create table Book(
 	price Double,
 	isbn varchar(255),
 	primary key (book_id)
-)
+);
 
 create table Bookstore(
 	store_address varchar(255) not null,
 	store_name varchar(255),
 	primary key (store_address)
-)
+);
 
 create table Buy (
 	book_id int not null,
@@ -29,25 +32,25 @@ create table Buy (
 	primary key (book_id, customer_id),
 	foreign key (book_id) references Book(book_id),
 	foreign key (customer_id) references Customer(customer_id)
-)
+);
 
 create table Customer(
 	customer_id int not null,
 	customer_name varchar(255), 
 	primary key (customer_id)
-)
+);
 
 create table Publisher(
 	publisher_address varchar(255) not null,
 	publisher_name varchar(255),
 	primary key (publisher_address)
-)
+);
 
 create table Salesman(
 	salesman_id int not null,
 	salesman_name varchar(255), 
 	primary key (salesman_id)
-)
+);
 
 
 /* Relation */
@@ -59,7 +62,7 @@ create table Has (
 	primary key (book_id, store_address),
 	foreign key (book_id) references Book(book_id),
 	foreign key (store_address) references Bookstore(store_address)
-)
+);
 
 create table Buy (
 	book_id int not null,
@@ -69,7 +72,7 @@ create table Buy (
 	primary key (book_id, customer_id),
 	foreign key (book_id) references Book(book_id),
 	foreign key (customer_id) references Customer(customer_id)
-)
+);
 
 create table Sell(
 	salesman_id int not null,
@@ -79,7 +82,7 @@ create table Sell(
 	foreign key (salesman_id) references Salesman(salesman_id),
 	foreign key (book_id) references Book(book_id)
     
-)
+);
 
 create table Work (
 	salesman_id int not null,
@@ -89,7 +92,7 @@ create table Work (
 	primary key (salesman_id, store_address),
 	foreign key (salesman_id) references Salesman(salesman_id),
 	foreign key (store_address) references Bookstore(store_address)
-)
+);
 
 create table Publish(
 	book_id int not null,
@@ -100,7 +103,7 @@ create table Publish(
 	foreign key (book_id) references Book(book_id),
 	foreign key (publisher_address) references Publisher(publisher_address)
 	on delete cascade
-)
+);
 
 create table Writes (
 	book_id int not null,
@@ -110,4 +113,4 @@ create table Writes (
 	primary key (book_id, author_id),
 	foreign key (book_id) references Book(book_id),
 	foreign key (author_id) references Author(author_id)
-)
+);
